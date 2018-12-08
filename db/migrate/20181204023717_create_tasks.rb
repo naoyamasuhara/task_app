b52c53f6dfd89b05423d9c27c3f1d9f89c1567b3
daikitakaya <takaya@sonicgarden.jp>
2018-12-08 16:59:06 +0900

    reviewコメント追加

--- db/migrate/20181204023717_create_tasks.rb
+++ db/migrate/20181204023717_create_tasks.rb
@@ -1,9 +1,18 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :content
       t.timestamps
     end
   end
 end
+
+# マイグレーションファイルとは
+# DBの設計書
+
+# dbを前のバージョンに戻すコマンド
+# rails db:rollback
+# マイグレーションファイルにnull: falseをつける
+# マイグレーションファイルを反映(もしくは実行するコマンド)
+# rails db:migrate
